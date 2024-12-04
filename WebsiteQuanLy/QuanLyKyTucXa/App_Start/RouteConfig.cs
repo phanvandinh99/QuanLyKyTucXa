79600e72621870d667eee979527fc09824c7502b
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2024-12-04 22:13:29 +0700

    UUpdate router default admin

--- WebsiteQuanLy/QuanLyKyTucXa/App_Start/RouteConfig.cs
+++ WebsiteQuanLy/QuanLyKyTucXa/App_Start/RouteConfig.cs
@@ -9,26 +9,12 @@ namespace QuanLyKyTucXa
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { Controller = "DangNhap", action = "Login", id = UrlParameter.Optional },
-            //    namespaces: new string[] { "QuanLyKyTucXa.Areas.DangNhap.Controllers" }
-            //).DataTokens.Add("area", "DangNhap");
-
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { Controller = "Home", action = "Index", id = UrlParameter.Optional },
-            //    namespaces: new string[] { "QuanLyKyTucXa.Areas.Admin.Controllers" }
-            //).DataTokens.Add("area", "Admin");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { Controller = "Home", action = "Index", id = UrlParameter.Optional },
-                namespaces: new string[] { "QuanLyKyTucXa.Areas.Student.Controllers" }
-            ).DataTokens.Add("area", "Student");
+                namespaces: new string[] { "QuanLyKyTucXa.Areas.Admin.Controllers" }
+            ).DataTokens.Add("area", "Admin");
         }
     }
 }
